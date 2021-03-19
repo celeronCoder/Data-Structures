19dd1fd405f134bff57adbaf190b5606e6d936b6
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-03-20 00:56:06 +0530

    renamed linked-list
    replaced underscore with a hyphen

--- linked_list/examples/deleteNode.py
+++
@@ -1,8 +0,0 @@
-from .createList import createList;
-
-if __name__=='__main__':
-	myList = createList(45,54534,13242315);
-	myList.traverse();
-	print("\n");
-	myList.deleteNode(13242315);
-	myList.traverse();
